2f6c03d17e0be6191a78961dce18c03eaf9ae4b1
junmer <junmer@foxmail.com>
2014-05-26 19:35:09 +0800

    add examples

--- examples/login/app.js
+++ examples/login/app.js
@@ -1,10 +1,18 @@
 var express = require('express')
   , passport = require('passport')
   , util = require('util')
-  , GitHubStrategy = require('passport-github').Strategy;
+  , BongStrategy = require('../../lib').Strategy;
 
-var GITHUB_CLIENT_ID = "--insert-github-client-id-here--"
-var GITHUB_CLIENT_SECRET = "--insert-github-client-secret-here--";
+
+
+/* test conf start */
+var BONG_HOST = 'http://open-test.bong.cn';
+var BONG_CLIENT_ID = "--insert-bong-client-id-here--"
+var BONG_CLIENT_SECRET = "--insert-bong-client-secret-here--";
+var BONG_TOKEN_URL = BONG_HOST + '/oauth/token';
+var BONG_AUTHORIZATION_URL = BONG_HOST + '/oauth/authorize';
+var BONG_USERPROFILE_URL = BONG_HOST + '/1/userInfo/{$uid}';
+/* test conf end */
 
 
 // Passport session setup.
@@ -12,7 +20,7 @@ var GITHUB_CLIENT_SECRET = "--insert-github-client-secret-here--";
 //   serialize users into and deserialize users out of the session.  Typically,
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete GitHub profile is serialized
+//   have a database of user records, the complete Bong profile is serialized
 //   and deserialized.
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -23,22 +31,24 @@ passport.deserializeUser(function(obj, done) {
 });
 
 
-// Use the GitHubStrategy within Passport.
+// Use the BongStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and GitHub
+//   credentials (in this case, an accessToken, refreshToken, and Bong
 //   profile), and invoke a callback with a user object.
-passport.use(new GitHubStrategy({
-    clientID: GITHUB_CLIENT_ID,
-    clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+passport.use(new BongStrategy({
+    authorizationURL: BONG_AUTHORIZATION_URL,
+    userProfileURL: BONG_USERPROFILE_URL,
+    tokenURL: BONG_TOKEN_URL,
+    clientID: BONG_CLIENT_ID,
+    clientSecret: BONG_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/bong/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      
-      // To keep the example simple, the user's GitHub profile is returned to
+      // To keep the example simple, the user's Bong profile is returned to
       // represent the logged-in user.  In a typical application, you would want
-      // to associate the GitHub account with a user record in your database,
+      // to associate the Bong account with a user record in your database,
       // and return that user instead.
       return done(null, profile);
     });
@@ -54,7 +64,7 @@ var app = express.createServer();
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
-  app.use(express.logger());
+  // app.use(express.logger());
   app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -80,25 +90,25 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-// GET /auth/github
+// GET /auth/bong
 //   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in GitHub authentication will involve redirecting
-//   the user to github.com.  After authorization, GitHubwill redirect the user
-//   back to this application at /auth/github/callback
-app.get('/auth/github',
-  passport.authenticate('github'),
+//   request.  The first step in bong authentication will involve redirecting
+//   the user to bong.com.  After authorization, bongwill redirect the user
+//   back to this application at /auth/bong/callback
+app.get('/auth/bong',
+  passport.authenticate('bong'),
   function(req, res){
-    // The request will be redirected to GitHub for authentication, so this
+    // The request will be redirected to bong for authentication, so this
     // function will not be called.
   });
 
-// GET /auth/github/callback
+// GET /auth/bong/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
+app.get('/auth/bong/callback', 
+  passport.authenticate('bong', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
@@ -109,6 +119,8 @@ app.get('/logout', function(req, res){
 });
 
 app.listen(3000);
+
+console.log('server start at:', 'http://127.0.0.1:3000');
 
 
 // Simple route middleware to ensure user is authenticated.
